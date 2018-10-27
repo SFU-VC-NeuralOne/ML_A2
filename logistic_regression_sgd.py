e2aac0ae1bc5f043e593958f9e34544f0c104664
Yijun Zhou <yijunzhou88@hotmail.com>
2018-10-27 14:15:57 -0700

    FInishing up

--- logistic_regression_sgd.py
+++ logistic_regression_sgd.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import assignment2 as a2
 
 # Maximum number of iterations.  Continue until this limit, or when error change is below tol.
-max_iter = 5000
+max_epoch = 500
 tol = 0.00001
 
 # Step size for gradient descent.
@@ -44,58 +44,43 @@ plt.rcParams.update({'font.size': 15})
 # plt.xlabel('slope')
 # plt.ylabel('intercept')
 # plt.axis([-5, 5, -10, 0])
-# plt.figure()
+plt.figure()
 for i in range(0, len(eta_list)) :
     # Initialize w.
     w = np.array([0.1, 0, 0])
-    number = np.arange(0, t.shape[0])
-    for iter in range(0, max_iter):
-        sgd_order = random.choice(number)
-        # Compute output using current w on all data X.
-        y = sps.expit(np.dot(X[sgd_order], w))
-        print(sgd_order)
-        t_sgd = t[sgd_order].copy()
-        # e is the error, negative log-likelihood (Eqn 4.90)
-        e = -(np.multiply(t_sgd, np.log(y)) + np.multiply((1 - t_sgd), np.log(1 - y)))
-
-        # Add this error to the end of error vector.
+    for epoch in range(0, max_epoch):
+        sgd_order = np.arange(0, t.shape[0])
+        random.shuffle(sgd_order)
+        e=0.0
+        for itr in sgd_order:
+            # Compute output using current w on all data X.
+            y_sgd = sps.expit(np.dot(X[itr], w))
+            t_sgd = t[itr]
+            #print('y_sgd & t_sgd', y_sgd,t_sgd)
+            # e is the error, negative log-likelihood (Eqn 4.90)
+            if(y_sgd==1 or y_sgd==0 ):
+                temp = 0
+            else:
+                temp = -(np.multiply(t_sgd, np.log(y_sgd)) + np.multiply((1 - t_sgd), np.log(1 - y_sgd)))
+            #print('temp',temp)
+            e = e+temp
+            #e = -np.mean(np.multiply(t, np.log(y)) + np.multiply((1 - t), np.log(1 - y)))
+            # Gradient of the error, using Eqn 4.91
+            grad_e = np.multiply((y_sgd - t_sgd), X[itr].T)
+            #grad_e = np.mean(np.multiply((y - t), X.T), axis=1)
+            # Update w, *subtracting* a step in the error derivative since we're minimizing
+            w_old = w
+            w = w - eta_list[i] * grad_e
+        e = e/t.shape[0]+1
         e_all[i].append(e)
-
-        # Gradient of the error, using Eqn 4.91
-        grad_e = np.multiply((y - t_sgd), X[sgd_order].T)
-
-        # Update w, *subtracting* a step in the error derivative since we're minimizing
-        w_old = w
-        w = w - eta_list[i] * grad_e
-
-        # Plot current separator and data.  Useful for interactive mode / debugging.
-        # plt.figure(DATA_FIG)
-        # plt.clf()
-        # plt.plot(X1[:,0],X1[:,1],'b.')
-        # plt.plot(X2[:,0],X2[:,1],'g.')
-        # a2.draw_sep(w)
-        # plt.axis([-5, 15, -10, 10])
-
-        # Add next step of separator in m-b space.
-        # plt.figure(SI_FIG)
-        # a2.plot_mb(w, w_old)
-
-        # Print some information.
-        print('epoch {0:d}, negative log-likelihood {1:.4f}, w={2}'.format(iter, e, w.T))
-        number = np.delete(number, (number == sgd_order).nonzero())
-        if(len(number)==0):
-            number = np.arange(0, t.shape[0])
-        # Stop iterating if error doesn't change more than tol.
-        if iter > 0:
-            if np.absolute(e - e_all[i][iter - 1]) < tol:
+        if epoch > 0:
+            if np.absolute(e - e_all[i][epoch - 1]) < tol:
                 break
-
-    # Plot error over iterations
-    # plt.figure()
+        print('epoch {0:d}, negative log-likelihood {1:.4f}, w={2}'.format(epoch, e, w.T))
     plt.plot(e_all[i])
-    plt.legend(['eta=0.5','eta=0.3','eta=0.1','eta=0.05','eta=0.01'])
 
-    # plt.legend(legend)
+
+plt.legend(['eta=0.5','eta=0.3','eta=0.1','eta=0.05','eta=0.01'])
 
 plt.ylabel('Negative log likelihood')
 plt.title('Training logistic regression')
